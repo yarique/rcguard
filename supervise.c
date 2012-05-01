371f2fd7ddcca357193c3f5eb7f32a53b727d5bb
Yar Tykhiy <yar@san4.int.barnet.com.au>
2012-05-01 13:38:58 +1000

    Start the project: A simple 1-thread supervisor for crash-prone daemons.

---
+++ supervise.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <err.h>
+#include <stdio.h>
+#include <sysexits.h>
+
+int
+main(int argc, char **argv)
+{
+	if (argc < 2) {
+	}
+}
